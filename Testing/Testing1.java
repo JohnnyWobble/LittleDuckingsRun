fe9dccb3d6da5ad29ac69d56106a1ce8539e5a2b
JohnnyWobble <46848052+JohnnyWobble@users.noreply.github.com>
2019-10-13 22:36:45 -0400

    bmrv1

--- Testing/Testing1.java
+++ Testing/Testing1.java
@@ -2,25 +2,10 @@ import java.util.Scanner;
 
 public class Testing1 {
     public static void main(String[ ] args) {
-        // Scanner in = new Scanner(System.in);/
-        Scanner in = new Scanner(System.in);
-        System.out.print("Please enter your name (first last): ");
-        String firstName = in.next();
-        System.out.println(firstName);
-        
+        String genderString = "Female";
+        char gender = genderString.charAt(0);
+        boolean isFemale = gender == 'F';
+        // int age = Integer.parseInt(ageString);
 
-        // System.out.println("Please enter a sentence: ");
-        // String firstWord = in.next();
-        // String restOfLine = in.nextLine();
-        // System.out.println("First word: " + firstWord);
-        // System.out.println("Rest of sentence: " + restOfLine);
-        // String sentence = firstWord + restOfLine;
-        // System.out.println("Whole sentence: " + sentence);
-        // print("'" + restOfLine + "'");
-        
-    }
-
-    public static void print(String str) {
-        System.out.println(str);
     }
 }
