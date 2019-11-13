f082d92827182ddf9d6cecf79a47adc06aa23c10
Johnny Wobble <46848052+JohnnyWobble@users.noreply.github.com>
2019-11-12 19:39:22 -0500

    updated codes

--- Testing/Testing1.java
+++ Testing/Testing1.java
@@ -1,14 +1,39 @@
-import java.util.Scanner;   
+import java.util.Random;
+import java.util.Scanner;  
+import java.lang.reflect.Array; 
 
 public class Testing1 {
     public static void main(String[ ] args) {
-        // int[] arrNum = {1, 2, 3, 4, 5, 6};
-        double [] numList = {4.17, 3.11, 2.46, 1.57};
-        int x = 0;
-        for(double n : numList)
-        {
-           x += (int)n + 1;
+
+        double times[] = {20.3, 22.5, 28.5, 24.6, 19.8, 26.7, 25.5};
+
+        double average = 0.0;
+        double total = 0.0;
+        
+        double min = Integer.MAX_VALUE;
+        doutble max = Integer.MIN_VALUE;
+        
+        for (int i = 0; i < times.length; i++) {
+        
+        if (min > times[i]) {
+        min = times[i];
+        } else if (max < times[i]) {
+        max = times[i];
         }
-        System.out.println(x);
+        }
+        
+        System.out.println("Your times, parsed for maximum and minimum are: ");
+        
+        for (double time : times) {
+        
+        if (time != min && time != max) {
+        System.out.print(time + " ");
+        total += time;
+        }
+        }
+        System.out.println("\nAnd your average was " + (total / (times.length - 2)));
+        
+        
+
     }
 }
