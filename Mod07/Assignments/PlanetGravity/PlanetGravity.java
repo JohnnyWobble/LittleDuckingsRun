70177f666877f3df76ab92d1f0e7fa7daeedcd83
Johnny Wobble <46848052+JohnnyWobble@users.noreply.github.com>
2019-11-16 18:52:26 -0500

    planet stuff

---
+++ Mod07/Assignments/PlanetGravity/PlanetGravity.java
@@ -0,0 +1,77 @@
+/**
+ * This will calculate the surface gravity for several planets
+ * 
+ * @author Max Gordon
+ * @version 11/14/2019
+ */
+
+import java.io.PrintWriter;
+import java.io.File;
+import java.io.IOException;
+
+public class PlanetGravity {
+    public static void main(String[] args) throws IOException {
+
+        // Define variables
+        String [] planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
+        double [] diameter = {4880, 12103.6, 12756.3, 6794, 142984, 120536, 51118, 49532};
+        double [] mass = {3.30e23, 4.869e24, 5.972e24, 6.4219e23, 1.900e27, 5.68e26, 8.683e25, 1.0247e26};
+
+        // Get gravity data
+        double[] gravity = calcGravity(diameter, mass);
+
+        // Displays data to the user
+        displayData(planets, diameter, mass, gravity);
+
+        // Prints data to a file
+        writeToFile(gravity);
+    }
+
+    public static double[] calcGravity(double[] diameter, double[] mass) {
+
+        // Define array length
+        double [] gravity = new double[mass.length];
+
+        // Iteratively calculate the data
+        double G = 6.67408e-11;
+        for (int i = 0; i < mass.length; i++) {
+            gravity[i] = (G * mass[i]) / Math.pow(diameter[i] * 1000 / 2, 2);
+        }
+
+        return gravity;
+    }
+
+    public static void displayData(String[] planets, double[] diameter, double[] mass, double[] gravity) {
+        
+        // Print headers
+        System.out.println("Planet      Diameter (km)      Mass (kg)      Gravity (m/s^2)");
+        System.out.println("_____________________________________________________________");
+
+        // Iteratively print data
+        for (int i = 0; i < mass.length; i++) {
+            System.out.printf("%-12s%10.0f         %-16.2E%8.2f%n", planets[i], diameter[i], mass[i], gravity[i]);
+        }
+    }
+
+    public static void writeToFile(double[] gravity) throws IOException {
+
+        // Create file type
+        File outFile = new File("SurfaceGravity.txt");
+
+        // Create file
+        outFile.createNewFile();
+
+        // Create printWriter
+        PrintWriter out = new PrintWriter(outFile);
+
+
+
+        // Write to file
+        for (double num : gravity) {
+            out.println(num);
+        }
+
+        // Close File
+        out.close();
+    }
+}
--- Testing/Testing1.java
+++ Testing/Testing1.java
@@ -5,35 +5,22 @@ import java.lang.reflect.Array;
 public class Testing1 {
     public static void main(String[ ] args) {
 
-        double times[] = {20.3, 22.5, 28.5, 24.6, 19.8, 26.7, 25.5};
+        // String[] str = {"asdfa", "dadafdfasdfadsf", "ads", "ad"};
 
-        double average = 0.0;
-        double total = 0.0;
-        
-        double min = Integer.MAX_VALUE;
-        doutble max = Integer.MIN_VALUE;
-        
-        for (int i = 0; i < times.length; i++) {
-        
-        if (min > times[i]) {
-        min = times[i];
-        } else if (max < times[i]) {
-        max = times[i];
-        }
-        }
-        
-        System.out.println("Your times, parsed for maximum and minimum are: ");
-        
-        for (double time : times) {
-        
-        if (time != min && time != max) {
-        System.out.print(time + " ");
-        total += time;
-        }
-        }
-        System.out.println("\nAnd your average was " + (total / (times.length - 2)));
-        
-        
+        int num = someMethod({"asdfa", "dadafdfasdfadsf", "ads", "ad"});
 
+        System.out.println(num);
+    }
+
+    public static int someMethod(String[] str) {
+
+        int min = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < str.length; i++) {
+            if (min > str[i].length()) {
+            min = str[i].length();
+            }
+        }
+        return min;
     }
 }
