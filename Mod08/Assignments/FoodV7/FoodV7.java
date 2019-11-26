95e62842c5e6f63f1f1d5cbbdb3547ce46f7a6e5
Johnny Wobble <46848052+JohnnyWobble@users.noreply.github.com>
2019-11-26 14:15:40 -0500

    Finishd FoodV7

--- Mod08/Assignments/FoodV7/FoodV7.java
+++ Mod08/Assignments/FoodV7/FoodV7.java
@@ -1,5 +1,5 @@
 /**
- * This will define some tasty
+ * This will define some tasty food
  * 
  * @author Max Gordon
  * @version 11/22/2019
@@ -12,19 +12,21 @@ public class FoodV7 {
 
 
     public FoodV7(String food) {
+        // Constructor for the object
         foodType = food;
     }
 
     public void setAmount(int amount) {
-        
+        // Sets the amount of units and edits the order ID
         if (amount <= 0) {
             amount = 1;
         }
-        orderID = foodType + amount;   
+        orderID = amount + foodType;   
         amountOfFood = amount;
     }
 
     public void setPrice(double price) {
+        // Sets the price per unit, edits the order ID, and calculates the total cost
         if (price <= 0) {
             price = 1.0;
         }
@@ -34,6 +36,7 @@ public class FoodV7 {
     }
 
     public void setPrice(int price) {
+        // Sets the price per unit, edits the order ID, and calculates the total cost
         if (price <= 0) {
             price = 1;
         }
@@ -43,21 +46,25 @@ public class FoodV7 {
     }
 
     public void printHeader() {
-        String header = "Name      Cost per Item   Items     Total Cost\n";
-        String line  =  "______________________________________________";
+        // Prints the header used in the receipt
+        String header = "Name      Cost per Item   Items     Total Cost        Order ID\n";
+        String line  =  "______________________________________________________________";
         System.out.println(header + line);
     }
 
     public void printReceipt() {
-        String dataF =  "%-10s%10.2f%11d%15.2f\n";
-        System.out.printf(dataF, foodType, costPerFood, amountOfFood, totalCost);
+        // Prints the receipt for the purchase
+        String dataF =  "%-10s%10.2f%11d%15.2f%16s\n";
+        System.out.printf(dataF, foodType, costPerFood, amountOfFood, totalCost, orderID);
     }
 
     public double getCost() {
+        // Returns the cost of the order
         return totalCost;
     }
 
     public String getOrderID() {
+        // Returns the order ID
         return orderID;
     }
 }
--- Mod08/Assignments/FoodV7/FoodV7Tester.java
+++ Mod08/Assignments/FoodV7/FoodV7Tester.java
@@ -7,10 +7,32 @@
 
 public class FoodV7Tester {
     public static void main(String[] args) {
-        FoodV7 food1 = new FoodV7("Taco");
-        food1.setAmount(4);
-        food1.setPrice(12.99);
-        food1.printHeader();
-        food1.printReceipt();
+        // Initializes objects
+        FoodV7 taco = new FoodV7("Taco");
+        FoodV7 pizza = new FoodV7("Pizza");
+        FoodV7 cereal = new FoodV7("Cereal");
+
+        // Set data for taco
+        taco.setAmount(4);
+        taco.setPrice(12.99);
+
+        // Sets data for pizza
+        pizza.setAmount(8);
+        pizza.setPrice(2);
+
+        // Sets data for cereal
+        cereal.setAmount(2);
+        cereal.setPrice(5.59);
+
+        // Prints the header, does not necessarily have to be from the taco object
+        taco.printHeader();
+
+        // Prints the receipts for all of the objects
+        taco.printReceipt();
+        pizza.printReceipt();
+        cereal.printReceipt();
+
+        // Calculates and prints the total cost
+        System.out.printf("\n   Final Cost: $%-6.2f\n", (taco.getCost() + pizza.getCost() + cereal.getCost()));
     }
 }
