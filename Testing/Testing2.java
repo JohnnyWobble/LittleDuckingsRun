f27d5f110d948d39bebc2212dbb09fbda4efabd0
Johnny Wobble <46848052+JohnnyWobble@users.noreply.github.com>
2019-12-17 08:48:26 -0500

    Made some triangle testing code

--- Testing/Testing2.java
+++ Testing/Testing2.java
@@ -1,27 +1,35 @@
 /**
- * <desciption>
+ * Testing different logical operators when calculating triangles
  * 
  * @author Max Gordon
- * @version 12/09/2019
+ * @version 12/12/2019
  */
 
 public class Testing2 {
     public static void main(String[] args) {
-        System.out.println(calcTri(3, 5, 4));
-        System.out.println(calcTri(1, 1, 1));
-        System.out.println(calcTri(100, 100, 1));
-        System.out.println(calcTri(100, 100, 200));
-        // System.out.println(calcTri(side1, side2, side3));
+        System.out.println("--- With AND ---");
+        System.out.println(calcTriAnd(3, 5, 4));        // Right triangle, should be false
+        System.out.println(calcTriAnd(1, 1, 1));        // Equilateral triangle, should, be true
+        System.out.println(calcTriAnd(100, 100, 1));    // Acute triangle, should be true
+        System.out.println(calcTriAnd(100, 100, 200));  // Obtuse triangle, should be false
+        System.out.println("--- With OR ---");
+        System.out.println(calcTriOr(3, 5, 4));         // Right triangle, should be false
+        System.out.println(calcTriOr(1, 1, 1));         // Equilateral triangle, should, be true
+        System.out.println(calcTriOr(100, 100, 1));     // Acute triangle, should be true
+        System.out.println(calcTriOr(100, 100, 200));   // Obtuse triangle, should be false
+
         
     }
 
-    public static boolean calcTri(int side1, int side2, int side3) {
-        // System.out.println(Math.pow(side1, 2) + "   " + Math.pow(side2, 2) + Math.pow(side3, 2));
-        // System.out.println(Math.pow(side2, 2) + "   " + Math.pow(side1, 2) + Math.pow(side3, 2));
-        // System.out.println(Math.pow(side3, 2) + "   " + Math.pow(side2, 2) + Math.pow(side1, 2));
-        if (Math.pow(side1, 2) < Math.pow(side2, 2) + Math.pow(side3, 2)
-                && Math.pow(side2, 2) < Math.pow(side1, 2) + Math.pow(side3, 2)
-                && Math.pow(side3, 2) < Math.pow(side2, 2) + Math.pow(side1, 2)) {
+    public static boolean calcTriAnd(int side1, int side2, int side3) {
+        if (Math.pow(side1, 2) < Math.pow(side2, 2) + Math.pow(side3, 2) && Math.pow(side2, 2) < Math.pow(side1, 2) + Math.pow(side3, 2) && Math.pow(side3, 2) < Math.pow(side2, 2) + Math.pow(side1, 2)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean calcTriOr(int side1, int side2, int side3) {
+        if (Math.pow(side1, 2) < Math.pow(side2, 2) + Math.pow(side3, 2) || Math.pow(side2, 2) < Math.pow(side1, 2) + Math.pow(side3, 2) || Math.pow(side3, 2) < Math.pow(side2, 2) + Math.pow(side1, 2)) {
             return true;
         }
         return false;
