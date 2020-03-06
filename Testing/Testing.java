dda2088bff995eb728e4d2ce7fd08ca8cef99442
Johnny Wobble <46848052+JohnnyWobble@users.noreply.github.com>
2020-03-06 12:14:54 -0500

    Finished mod15

--- Testing/Testing.java
+++ Testing/Testing.java
@@ -1,60 +1,26 @@
+
 /**
  * 
  * @author Max Gordon
  * @version 01/14/2020
  */
+import java.util.ArrayList;
 
 public class Testing {
     public static void main(String[] args) {
-        // System.out.println(recur);
-        System.out.println(" hello ");
-        System.out.println(" hello \t".trim() + "---");
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        ArrayList b = a;
+        a.add(new Integer(4));
+        b.add(new Integer(5));
+        a.add(new Integer(6));
+        System.out.println(b.size());
     }
 
-    public static String recur(String str, int n) {
-        if (n >= str.length()) {
-            return str;
-        }
+    private int number;
 
-        return str + recur(str.substring(n), n + 2);
-    }
-
-    public static void recur(String str)
-    {
-        int len = str.length();
-        if(len > 1)
-        {
-             String temp = str.substring(0, len - 2);
-             recur(temp);
-             System.out.println(temp);
-        }
-   }
-
-   public static int recur(int n)
-{
-     if(n < 4)
-     {
-          return 2;
-     }
-     else
-     {
-          return recur(n / 4) + 2;
-     }
-}
-
-    public static int mystery(int n) {
-        if (n > 5) {
-            return mystery(n - 2) + 2;
-        }
-
-        return n + 2;
-    }
-
-    public static int mystery(int n, boolean a) {
-        if (n > 5) {
-            return mystery(n - 2, false) + 2;
-        }
-
-        return n + 2;
+    public void addNumbers(int number) {
+        number = number + number;
+        System.out.println("The local variable is: " + number);
+        System.out.println("The instance variable is: " + number);
     }
 }
